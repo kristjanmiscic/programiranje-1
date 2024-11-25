12bd0442c57f091cd5b183fddef1b32807ea95d9
kristjanmiscic <165583794+kristjanmiscic@users.noreply.github.com>
2024-11-25 17:30:58 +0000

    vaje

--- 07-odvisni-tipi/vaje/logic.lean
+++ 07-odvisni-tipi/vaje/logic.lean
@@ -1,7 +1,15 @@
 -- Izomorfizmi
 
 theorem eq1 {A B : Prop} : (A ∧ B) ↔ (B ∧ A) :=
-  sorry
+  by
+    -- Iff.intro
+    constructor
+    intro ab
+    constructor
+    exact ab.right
+    exact ab.left
+    intro ba
+    sorry
 
 theorem eq2 {A B : Prop} : (A ∨ B) ↔ (B ∨ A) :=
   sorry
@@ -16,7 +24,27 @@ theorem eq5 {A B C : Prop} : A ∧ (B ∨ C) ↔ (A ∧ B) ∨ (A ∧ C) :=
   sorry
 
 theorem eq6 {A B C : Prop} : (B ∨ C) → A ↔ (B → A) ∧ (C → A) :=
-  sorry
+  by
+    constructor -- za dokaz ekvivalence
+    intro h
+    constructor
+    intro b
+    apply h
+    left
+    exact b
+    intro c
+    have xx : B ∨ C := Or.inr c
+    have yy := h (Or.inr c)
+    exact yy
+    intro h BvC
+    cases BvC
+    case inl b =>
+      have l := h.left
+      apply h.left
+      exact b
+    case inr c =>
+      exact h.right c
+
 
 theorem eq7 {A B C : Prop} : C → (A ∧ B) ↔ (C → A) ∧ (C → B) :=
   sorry
